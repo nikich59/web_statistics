4003c8ea1c6e61f8693e2cef5dcdcf5014e16261
Евгений Стёпин <meow@MacBook-Pro-Evgenij.local>
2018-01-11 21:08:46 +0500

    test line chart

---
+++ visio/webapp/src/main/java/Point.java
@@ -0,0 +1,8 @@
+public class Point<A, B> {
+    public A x;
+    public B y;
+    public Point (A x, B y) {
+        this.x = x;
+        this.y = y;
+    }
+}
--- visio/webapp/src/main/java/TestServlet.java
+++ visio/webapp/src/main/java/TestServlet.java
@@ -1,24 +1,31 @@
+import com.google.gson.Gson;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-
-/**
- * Created by Nikita on 08.01.2018.
- */
+import java.text.SimpleDateFormat;
+import java.util.*;
 
 @WebServlet(
 		name = "test_servlet",
 		urlPatterns = "/test_servlet"
 )
-public class TestServlet extends HttpServlet
-{
+public class TestServlet extends HttpServlet {
 	@Override
-	protected void doGet( HttpServletRequest req, HttpServletResponse resp )
-			throws ServletException, IOException
-	{
-		resp.getWriter( ).write( "TEST" );
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		ArrayList<Point<String, Integer>> points = new ArrayList<Point<String, Integer>>();
+		SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+		Calendar calendar = new GregorianCalendar();
+		
+		
+		Random random = new Random();
+		for (int i = 0; i < 1000; i++) {
+			points.add(new Point<String, Integer>(sdf.format(calendar.getTime()), random.nextInt() % 1000));
+			calendar.add(Calendar.HOUR, 24);
+		}
+		response.getWriter().print(new Gson().toJson(points));
 	}
 }
